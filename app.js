ba7ae7ece584f62044820f4070bbf8bd58ea3b3d
Riza Fahmi <rizafahmi@gmail.com>
2018-04-22 10:48:28 +0700

    Nicer callback

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const { getProfile, getRepos, countTotalStars } = require("./helpers");
 
-getProfile("rizafahmi", (err, profile) => {
+const handleCountStars = (err, stars) => {
   if (err) console.error(err);
-  getRepos(JSON.parse(profile).items[0].login, (err, repos) => {
-    if (err) console.error(err);
-    countTotalStars(JSON.parse(repos), (err, stars) => {
-      if (err) console.error(err);
-      console.log(`Total stars: ${stars}`);
-    });
-  });
-});
+  console.log(`Total stars: ${stars}`);
+};
+const handleRepos = (err, repos) => {
+  if (err) console.error(err);
+  countTotalStars(JSON.parse(repos), handleCountStars);
+};
+const handleProfile = (err, profile) => {
+  if (err) console.error(err);
+  getRepos(JSON.parse(profile).items[0].login, handleRepos);
+};
+
+getProfile("rizafahmi", handleProfile);

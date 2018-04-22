8aba4f5def74f44caaf4f75ce63e9a793b409dc8
Riza Fahmi <rizafahmi@gmail.com>
2018-04-22 11:18:32 +0700

    Handle error with try/catch

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const { getProfile, getRepos, countTotalStars } = require("./helpers");
 
-const riza = getProfile("rizafahmi");
-const hengki = getProfile("hengkiardo");
-
-Promise.all([riza, hengki]).then(([rizaProfile, hengkiProfile]) => {
-  console.log(
-    rizaProfile.data.items[0].login,
-    hengkiProfile.data.items[0].login
-  );
-});
+(async () => {
+  try {
+    const profile = await getProfile("rizafahmi");
+    const repos = await getRepos(profile.data.items[0].login);
+    const stars = await countTotalStars(repos.data);
+    console.log(`Total stars: ${stars}`);
+  } catch (e) {
+    console.error(e);
+  }
+})();

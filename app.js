8dbf30ad4ec6ba70775c45ea15edcdce7c155e3e
Riza Fahmi <rizafahmi@gmail.com>
2018-04-22 11:22:14 +0700

    Handle error when we call it

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const { getProfile, getRepos, countTotalStars } = require("./helpers");
 
 (async () => {
-  try {
-    const profile = await getProfile("rizafahmi");
-    const repos = await getRepos(profile.data.items[0].login);
-    const stars = await countTotalStars(repos.data);
-    console.log(`Total stars: ${stars}`);
-  } catch (e) {
-    console.error(e);
-  }
+  const profile = await getProfile("rizafahmi").catch(e => console.error(e));
+  const repos = await getRepos(profile.data.items[0].login).catch(e =>
+    console.error(e)
+  );
+  const stars = countTotalStars(repos.data);
+  console.log(`Total stars: ${stars}`);
 })();

0bbc2cceb423171ebe4e839f50da500a6308a6fb
Riza Fahmi <rizafahmi@gmail.com>
2018-04-22 11:00:26 +0700

    Finish promise version

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 const { getProfile, getRepos, countTotalStars } = require("./helpers");
 
-const handleCountStars = (err, stars) => {
-  if (err) console.error(err);
-  console.log(`Total stars: ${stars}`);
-};
-const handleRepos = (err, repos) => {
-  if (err) console.error(err);
-  countTotalStars(JSON.parse(repos), handleCountStars);
-};
-const handleProfile = (err, profile) => {
-  if (err) console.error(err);
-  getRepos(JSON.parse(profile).items[0].login, handleRepos);
-};
-
-getProfile("rizafahmi", handleProfile);
+getProfile("rizafahmi")
+  .then(profile => getRepos(profile.data.items[0].login))
+  .then(repos => countTotalStars(repos.data))
+  .then(stars => console.log(`Total stars: ${stars}`))
+  .catch(err => console.error(err));
--- helpers.js
+++ helpers.js
@@ -1,36 +1,30 @@
-const request = require("request");
+const request = require("axios");
 
 const BASE_URL = `https://api.github.com`;
 
-const getProfile = (username, cb) => {
+const getProfile = username => {
   const options = {
     url: `${BASE_URL}/search/users?q=${username}`,
     headers: {
       "User-Agent": "request"
-    }
+    },
+    method: "GET"
   };
-  request(options, (err, resp, body) => {
-    if (err) cb(err);
-    cb(null, body);
-  });
+  return request(options);
 };
 
-const getRepos = (username, cb) => {
+const getRepos = username => {
   const options = {
     url: `${BASE_URL}/users/${username}/repos?per_page=100`,
     headers: {
       "User-Agent": "request"
-    }
+    },
+    method: "GET"
   };
-  request(options, (err, resp, body) => {
-    if (err) cb(err);
-    cb(null, body);
-  });
+  return request(options);
 };
 
-const countTotalStars = (repos, cb) => {
-  cb(null, repos.reduce((prev, curr) => prev + curr.stargazers_count, 0));
-};
+const countTotalStars = repos => repos.reduce((prev, curr) => prev + curr.stargazers_count, 0);
 
 module.exports = {
   getProfile,

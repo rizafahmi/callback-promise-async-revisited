158734be39d1b9bce57cd209df907b0622f5d031
Riza Fahmi <rizafahmi@gmail.com>
2018-04-22 11:06:41 +0700

    Async hell

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const { getProfile, getRepos, countTotalStars } = require("./helpers");
 
-getProfile("rizafahmi")
-  .then(profile => getRepos(profile.data.items[0].login))
-  .then(repos => countTotalStars(repos.data))
-  .then(stars => console.log(`Total stars: ${stars}`))
-  .catch(err => console.error(err));
+(async () => {
+  const riza = await getProfile("rizafahmi");
+  const hengki = await getProfile("hengkiardo");
+  console.log(riza.data.items, hengki.data.items);
+})();

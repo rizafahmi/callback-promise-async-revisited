405cfd7004a8559e61caa050ee62c7af8aec3364
Riza Fahmi <rizafahmi@gmail.com>
2018-04-22 11:40:24 +0700

    Finish higher order function

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const { getProfile, getRepos, countTotalStars } = require("./helpers");
 
-const handleError = fn => (...params) => fn(...params).catch(e => {
-      console.error(e);
-    });
+const handleError = fn => (...params) =>
+  fn(...params).catch(e => {
+    console.error(e);
+  });
 
 const safeGetProfile = handleError(getProfile);
-safeGetProfile("rizafahmi");
+(async () => {
+  const riza = await safeGetProfile("rizafahmi");
+  console.log(riza.data.items[0].login);
+})();

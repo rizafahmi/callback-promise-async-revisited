9998b4edff955a70e0facfb667dc56d5a2bb0e99
Riza Fahmi <rizafahmi@gmail.com>
2018-04-22 11:29:11 +0700

    Finish Higher Order Function

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const { getProfile, getRepos, countTotalStars } = require("./helpers");
 
-(async () => {
-  const profile = await getProfile("rizafahmi").catch(e => console.error(e));
-  const repos = await getRepos(profile.data.items[0].login).catch(e =>
-    console.error(e)
-  );
-  const stars = countTotalStars(repos.data);
-  console.log(`Total stars: ${stars}`);
-})();
+const handleError = fn => (...params) => fn(...params).catch(e => {
+      console.error(e);
+    });
+
+const safeGetProfile = handleError(getProfile);
+safeGetProfile("rizafahmi");

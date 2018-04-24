fe98a610b71c0c0f25f2883cdd2b61b43844f46b
Riza Fahmi <rizafahmi@gmail.com>
2018-04-24 17:14:30 +0700

    Async await

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const { getProfile, getRepos, countTotalStars } = require("./helpers");
 
 (async () => {
-  const riza = await getProfile("rizafahmi");
-  const hengki = await getProfile("hengkiardo");
-  console.log(riza.data.items, hengki.data.items);
+  const riza_profile = await getProfile("rizafahmi");
+  const riza_repos = await getRepos(riza_profile.login);
+  const riza_stars = countTotalStars(riza_repos);
+  console.log(riza_stars);
 })();
